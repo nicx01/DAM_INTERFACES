71e0bb610ec9262b122195d3ddb95a2c471a537a
Nico Saez <nico.ojer@gmail.com>
2024-10-03 20:36:52 +0200

    Ejercicio 1

---
+++ EjerciciosC#/Ejercicio1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Ejercicio1
+{
+    public void CantidadVocales()
+    {
+        Console.WriteLine("Por favor, ingrese una palabra:");
+        string palabra = Console.ReadLine();
+        char[] vocales = {'a', 'e', 'i', 'o', 'u'};
+        int numVocales = 0;
+        for (int i = 0; i < palabra.Length; i++)
+        {
+            if (Array.Exists(vocales, v => v == palabra[i]))
+            {
+                numVocales++;
+            }
+        }
+    Console.WriteLine("La palabra " + palabra + " tiene "+ numVocales +" vocales");
+    }
+}
--- EjerciciosC#/Program.cs
+++ EjerciciosC#/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EjerciciosInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+
+            do
+            {
+                Console.WriteLine("Seleccione un ejercicio o 0 para salir:");
+                Console.WriteLine("1. Ejercicio 1");
+                Console.WriteLine("2. Ejercicio 2");
+                Console.WriteLine("3. Ejercicio 3");
+                Console.Write("Ingrese su opción: ");
+
+                if (int.TryParse(Console.ReadLine(), out opcion))
+                {
+                    switch (opcion)
+                    {
+                        case 1:
+                            new Ejercicio1().CantidadVocales();
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 0:
+                            Console.WriteLine("Saliendo...");
+                            break;
+                        default:
+                            Console.WriteLine("Opción no válida, intente nuevamente.");
+                            return;
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Por favor, ingrese un número válido.");
+                }
+
+                Console.WriteLine();
+
+            } while (true);
+        }
+    }
+}
