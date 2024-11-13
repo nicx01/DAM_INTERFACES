631fc18b9eb30f466e91c8cf27402e1cf633e9fc
Nico Saez <nico.ojer@gmail.com>
2024-11-13 16:45:46 +0100

    Poke API exercise

---
+++ PokemonBackRules/Utils/Constantes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonBackRules.Utils
+{
+    public static class Constantes
+    {
+        public const string POKE_TYPE_URL= "https://pokeapi.co/api/v2/type/";
+        public const int MAX_POKE_ITEMS = 150;
+        public const string MISSINGNO_IMAGE_PATH = "../Resources/missingNo.jpg";
+    }
+}
