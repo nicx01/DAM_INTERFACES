875bdf14b95f3070697c227e01e4677d95d4d3b2
Nico Saez <nico.ojer@gmail.com>
2024-12-04 17:40:28 +0100

    pokemon can attack, die and have health

--- PokemonBackRules/Model/PokemonResponseModel.cs
+++ PokemonBackRules/Model/PokemonResponseModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace PokemonBackRules.Model
@@ -7,6 +8,9 @@ namespace PokemonBackRules.Model
     {
         [JsonPropertyName("sprites")]
         public FightSprites FightSprites { get; set; }
+
+        [JsonPropertyName("stats")]
+        public List<Stat> Stats { get; set; }
     }
 
     public class FightSprites
@@ -16,5 +20,22 @@ namespace PokemonBackRules.Model
 
         [JsonPropertyName("front_shiny")]
         public string FrontShiny { get; set; }
+    }
+
+    public class Stat
+    {
+        [JsonPropertyName("base_stat")]
+        public int BaseStat { get; set; }
+
+        [JsonPropertyName("stat")]
+        public StatInfo StatInfo { get; set; }
+    }
+
+    public class StatInfo
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("attack")]
+        public string Attack { get; set; }
     }
 }
--- PokemonBackRules/ViewModel/FightViewModel.cs
+++ PokemonBackRules/ViewModel/FightViewModel.cs
@@ -23,16 +23,18 @@ namespace PokemonBackRules.ViewModel
         private const string PokeApiUrl = "https://pokeapi.co/api/v2/pokemon/";
         private Random random = new Random();
 
+        [ObservableProperty]
         private string _pokemonImage;
-        public string PokemonImage
-        {
-            get => _pokemonImage;
-            set
-            {
-                _pokemonImage = value;
-                OnPropertyChanged(nameof(PokemonImage));
-            }
-        }
+        [ObservableProperty]
+        private int _opponentHealth;
+        [ObservableProperty]
+        private int _maxOpponentHealth = 100; //default
+        [ObservableProperty]
+        private int _playerHealth = 1000; 
+        [ObservableProperty]
+        private int _maxPlayerHealth = 1000; 
+        [ObservableProperty]
+        private int _pokemonAttack = 0;
 
         public FightViewModel()
         {
@@ -40,44 +42,67 @@ namespace PokemonBackRules.ViewModel
         }
 
         [RelayCommand]
-        private void Scape()
+        public void Scape()
         {
             LoadRandomPokemon();
         }
+        [RelayCommand]
 
-        private async void LoadRandomPokemon()
+        public void ApplyDamage()
+        {
+            Random random = new Random();
+            int damage = random.Next(0, 41); OpponentHealth -= damage;
+            if (OpponentHealth <= 0) LoadRandomPokemon();
+            else PlayerHealth -= PokemonAttack;
+        }
+        private async Task LoadRandomPokemon()
         {
             try
             {
-                int randomId = random.Next(1, 101); 
+                int randomId = random.Next(1, 101);
                 string apiUrl = $"{PokeApiUrl}{randomId}";
 
                 HttpResponseMessage response = await HttpClient.GetAsync(apiUrl);
-                if (response.IsSuccessStatusCode)
-                {
-                    string json = await response.Content.ReadAsStringAsync();
-                    var pokemonData = JsonSerializer.Deserialize<PokemonResponseModel>(json);
 
-                    bool useShinySprite = random.Next(0, 100) < 5;
-                    PokemonImage = useShinySprite ?
-                        pokemonData.FightSprites.FrontShiny ?? Constantes.MISSINGNO_IMAGE_PATH :
-                        pokemonData.FightSprites.FrontDefault ?? Constantes.MISSINGNO_IMAGE_PATH;
-
-                    }
-                    else
+                if (!response.IsSuccessStatusCode)
                 {
-                    PokemonImage = "..Resources\\missingNo.jpg";
+                    SetDefaultPokemonImage();
+                    return;
                 }
+
+                string json = await response.Content.ReadAsStringAsync();
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                var pokemonData = JsonSerializer.Deserialize<PokemonResponseModel>(json, options);
+
+                if (pokemonData == null)
+                {
+                    SetDefaultPokemonImage();
+                    return;
+                }
+
+                var hpStat = pokemonData.Stats.FirstOrDefault(s => s.StatInfo.Name == "hp");
+                var attackStat = pokemonData.Stats.FirstOrDefault(s => s.StatInfo.Name == "attack");
+                MaxOpponentHealth = hpStat?.BaseStat ?? 100;
+                OpponentHealth = MaxOpponentHealth;
+                PokemonAttack = attackStat?.BaseStat ?? 10;
+
+                bool useShinySprite = random.Next(0, 100) < 5;
+                PokemonImage = useShinySprite
+                    ? pokemonData.FightSprites.FrontShiny ?? Constantes.MISSINGNO_IMAGE_PATH
+                    : pokemonData.FightSprites.FrontDefault ?? Constantes.MISSINGNO_IMAGE_PATH;
             }
             catch (Exception ex)
             {
-                PokemonImage = "..Resources\\missingNo.jpg";
+                SetDefaultPokemonImage();
+                Console.WriteLine($"Error loading Pokémon: {ex.Message}");
             }
         }
 
-
-        private void PerformScape()
+        private void SetDefaultPokemonImage()
         {
+            PokemonImage = Constantes.MISSINGNO_IMAGE_PATH;
+            MaxOpponentHealth = 100;
+            OpponentHealth = MaxOpponentHealth;
         }
     }
 }
